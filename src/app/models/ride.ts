cfa8c8f796e304b5a724a6cd02350b58ed0c6778
Pawel Wiklowski <pawel.wiklowski@gmail.com>
2020-05-24 13:31:58 +0200

    Update ride model

--- src/app/models/ride.ts
+++ src/app/models/ride.ts
@@ -1,1 +1,15 @@
-export default interface Ride {}
+export default interface Ride {
+  fileId: string;
+  fileName: string;
+  _id: string;
+  metaData: MetaData;
+}
+
+export interface MetaData {
+  start: number;
+  tripTime: number;
+  tripDistance: number;
+  tripUsedEnergy: number;
+  maxSpped: number;
+  maxCurrent: number;
+}
